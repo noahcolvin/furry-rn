ad4cc61ec960e9168137f527453bbba3009f611b
Noah Colvin <7552226+noahcolvin@users.noreply.github.com>
2025-02-07 21:41:01 -0700

    Adding ad

--- app/index.tsx
+++ app/index.tsx
@@ -2,18 +2,19 @@ import AnimalList from '@/components/AnimalList';
 import ButtonList from '@/components/ButtonList';
 import MyFavoriteItems from '@/components/MyFavoriteItems';
 import MyFriendList from '@/components/MyFriendList';
-import { Text, View, StyleSheet, ScrollView } from 'react-native';
+import { Text, View, StyleSheet, ScrollView, Image } from 'react-native';
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <ScrollView>
+      <ScrollView showsVerticalScrollIndicator={false}>
         <Text style={styles.headerText}>Welcome back</Text>
         <ButtonList />
         <Text style={styles.headerText}>What furry friend brought you here today?</Text>
         <AnimalList />
         <Text style={styles.headerText}>Your favorites</Text>
         <MyFavoriteItems />
+        <Image source={require('../assets/images/pet_insurance.png')} style={styles.ad} />
         <Text style={styles.headerText}>Your furry friends</Text>
         <MyFriendList />
       </ScrollView>
@@ -34,5 +35,11 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginHorizontal: 8,
     marginTop: 4,
+  },
+  ad: {
+    width: '100%',
+    height: 270,
+    flex: 1,
+    resizeMode: 'contain',
   },
 });
