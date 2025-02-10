4d6fd3f3374a2d6e90d006ffce8ae510085cf133
Noah Colvin <7552226+noahcolvin@users.noreply.github.com>
2025-02-09 21:30:55 -0700

    Fix item detail state, mostly

--- app/item/[id].tsx
+++ app/item/[id].tsx
@@ -1,16 +1,21 @@
 import { useStoreItemData } from '@/features/storeItems/data/storeItemsStore';
 import StoreItemDetail from '@/features/storeItems/presentation/StoreItemDetail';
 import { useLocalSearchParams } from 'expo-router';
+import { useEffect } from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
 
 export default function Id() {
   const { id } = useLocalSearchParams();
-  const { data, error } = useStoreItemData();
+  const { data, success, fetchStoreItems } = useStoreItemData();
   const item = data?.find(item => item.id === id);
+
+  useEffect(() => {
+    fetchStoreItems({animal: 'all', product: 'all'});
+  }, []);
 
   return (
     <ScrollView showsVerticalScrollIndicator={false} style={styles.container}>
-      <StoreItemDetail item={item!} />
+      {success && item && <StoreItemDetail item={item!} />}
     </ScrollView>
   );
 }
